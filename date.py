a963eb3977d539f33c04e81481a85ff7fa537291
Kathryn Sarah Lutz <klutz1@nd.edu>
2017-03-20 08:42:37 -0400

    Kathryn Lutz executable code lab6: Python practice/capabilities, research other possible formats, questionnaire pt.2

--- date.py
+++ date.py
@@ -50,5 +50,22 @@ else:
   else:
     print 'Late night?'
 
+#Hash table research:
+#If we have a huge amount of movie and television titles, we need some method
+#of accessing each title with a hash algorithm or computation. So, if it
+#is the morning/afternoon/night, perhaps there is a way to organize each of 
+#the titles by most appropriate for that time of day. The real issue is whether
+#or not a movie, such as a comedy, can be placed in both the "morning" and
+#"afternoon" sections. We still don't know how hash tables work, but they 
+#seem like the most appropriate way of storing the amount of things we're 
+#working with. I'm also not quite sure how double hashing or linear probing
+#will be appropriate for what we want, too. 
 
+#What needs to happen first and foremost is that although Netflix updates their
+#TV and film selections every few days/weeks, it is much easier for us to 
+#work with the one set of selections from the very beginning such that the 
+#size of the library stays the same and the "spots", or calculated numbers for
+#each selection, stay the same throughout the entire project.I think this 
+#will save us quite a bit of time in the long run and will hopefully aid us
+#in our use of a hash table for the first time.
 
